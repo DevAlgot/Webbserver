d7f96f2dc9700f992395d027c830efc49882a598
Algot Grönvall <algot.gronvall@hotmail.com>
2025-08-18 23:03:54 +0200

    css!!!!!!!

--- docker-lemp-main/www/public/evaluate.php
+++ docker-lemp-main/www/public/evaluate.php
@@ -1,20 +1,61 @@
 <!doctype html>
 <html>
-<head lang="sv"></head>
+
+<head lang="sv">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
 <body>
-<h1>Matematik-test</h1>
-<?php
-    $ans = $_POST['q1'];
-    $quest = $_POST['b1'];
-    $points = 0;
-    echo("<p>" . $quest . "</p>");
+    <main>
+        <h1>Matematik-test</h1>
 
-    if ($ans == $_POST['b1']) {
-        $points++;
-    }
-    echo("<p> Du fick " . $points . " poäng!</p>");
-?>
+        <?php
+        $name = $_POST['name'];
 
+        $questionsAmount = 15; // Antal frågor
+
+
+        $ans = array();
+        for ($i = 1; $i <= $questionsAmount; $i++) {
+            $ans[] = $_POST['q' . $i];
+            $cor = explode(',', $_POST['a' . $i]);
+            
+            if (($ans[$i - 1] == $cor[0])) {
+                echo ("
+                <fieldset> 
+                    <Legend>Fråga " . $i . "</Legend> 
+                    <b> " . $cor[1] . "</b> 
+                    <br>
+                    <p>Ditt svar: "  . $ans[$i - 1] . "</p> 
+                </fieldset>");
+            } else {
+                echo ("
+                <fieldset class='wrong'> 
+                    <Legend>Fråga " . $i . " </Legend> 
+                    <b> " . $cor[1] . "</b> 
+                    <br>
+                    <p>Ditt svar: "  . $ans[$i - 1] . "</p> 
+                    <label>Rätt svar: </label> " .  $cor[0] . "
+                </fieldset>");
+            }
+        }
+
+
+        $points = 0;
+        for ($i = 1; $i <= $questionsAmount; $i++) {
+            if ($ans[$i - 1] == $cor[0]) {
+                $points++;
+            }
+        }
+
+
+        echo ("<p>" . $name . " fick " . $points . " poäng!</p>");
+        ?>
+    </main>
 
 </body>
+
 </html>
--- docker-lemp-main/www/public/script.js
+++ docker-lemp-main/www/public/script.js
@@ -2,9 +2,13 @@ function start(){
     let quest = document.getElementById("questions");
     let questions = "";
 
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 15; i++) {
+        let random = `${getRandomArbitrary(1, 10)} + ${getRandomArbitrary(1, 10)}`;
+        let evalResult = eval(random);
+
         quest.innerHTML += `
-        <label name ="b${i+1}">${getRandomArbitrary(1, 10)} + ${getRandomArbitrary(1, 10)} = </label>
+        <label name="ItemName">${random} = </label>
+        <input type="hidden" name="a${i+1}" value="${evalResult}, ${random}">
         <input type="text" name="q${i+1}">
        <br>
        `
